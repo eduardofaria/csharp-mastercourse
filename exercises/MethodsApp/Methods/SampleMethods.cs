b8011d0eb1ea3ccd2c184afbd4f11fcaa3df7d25
Eduardo Faria <eduardofaria.rj@gmail.com>
2024-02-26 13:30:14 -0300

    Feat: Understanding Class Libraries And References

---
+++ exercises/MethodsApp/Methods/SampleMethods.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+/*
+namespace Methods;
+   internal static class SampleMethods
+   {
+ */
+
+
+namespace Methods
+{
+    internal static class SampleMethods
+    {
+    }
+}
